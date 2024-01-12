7fe932cc8f7e3db89a22db6a064a92ea65899285
Jean Carlo Quiroga Hernandez <jean.quiroga@ci2.co>
2024-01-12 07:41:25 -0500

    modificacion de warnings

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import "./App.css";
 import { useFetch } from "./api/datos";
+import { app } from "./strings";
 import "./api/styles.css";
 
 function App() {
   //recibo la información obtenida de la función y se envía la url del api
   const { data } = useFetch(
-    "https://raw.githubusercontent.com/ci2Juan/api-client/main/users.json" 
+    "https://raw.githubusercontent.com/ci2Juan/api-client/main/users.json"
   );
 
   //se almacena el mapeo de los datos del json en la variable cards
   const cards = data?.map((item) => (
     <>
       <div>
-        <div className="imagenT"><img className="imagen" src={item.image}/></div>
+        <div className="imagenT"><img className="imagen" src={item.image} alt="logo" /></div>
         <div className="informacion">
-        <p key={item.name}><span>Name:</span> {item.name}</p>
-        <p key={item.gender}><span>Gender:</span> {item.gender}</p>
-        <p key={item.height}><span>Height:</span> {item.height}</p>
-        <p key={item.eye_color}><span>Eye color:</span> {item.eye_color}</p>
+          <p key={item.name}><span>{app.nameTitle}</span> {item.name}</p>
+          <p key={item.gender}><span>Gender:</span> {item.gender}</p>
+          <p key={item.height}><span>Height:</span> {item.height}</p>
+          <p key={item.eye_color}><span>Eye color:</span> {item.eye_color}</p>
         </div>
       </div>
     </>
-  )); 
+  ));
 
   return (
     <div>
       <div className="titulo"><h1>User Data</h1></div>
       <div className="columnas">
-      {cards?.map((card, id) => (
-        <div key={id} className="tarjeta">
-          {card}
-        </div>
-      ))}
-    </div>
+        {cards?.map((card, id) => (
+          <div key={id} className="tarjeta">
+            {card}
+          </div>
+        ))}
+      </div>
     </div>//mapeo la constante cards, pasando un id único para que tenga un identificador diferente en cada tarjeta 
   );
 }
